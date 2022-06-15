254459d6b5513f70a206ad4316466517daffd008
Linh B Ngo <lngo@wcupa.edu>
2022-06-15 18:18:40 -0500

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,12 @@
+import geni.portal as portal
+import geni.rspec.pg as rspec
+
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+# Create a XenVM
+node = request.XenVM("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node.routable_control_ip = "true"
+
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec()
